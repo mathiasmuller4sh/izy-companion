c3d85e03ac6e88b735411f09334d95ffd0417753
Mathias Muller <mathias.muller@4sh.fr>
2015-01-14 23:21:38 +0100

    use modules

---
+++ www/app.js
@@ -0,0 +1,90 @@
+angular.module('izy-companion', ['ionic', 'izy-companion.home'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+    url: "/",
+    abstract: true,
+    templateUrl: "modules/global/layout.html"
+  })
+//
+//  // Each tab has its own nav history stack:
+//
+//  .state('home.root', {
+//    url: '/home',
+//    views: {
+//      'tab-dash': {
+//        templateUrl: 'modules/home/home.html',
+//        controller: 'HomeCtrl'
+//      }
+//    }
+//  })
+//
+//  .state('tab.chats', {
+//      url: '/chats',
+//      views: {
+//        'tab-chats': {
+//          templateUrl: 'templates/tab-chats.html',
+//          controller: 'ChatsCtrl'
+//        }
+//      }
+//    })
+//    .state('tab.chat-detail', {
+//      url: '/chats/:chatId',
+//      views: {
+//        'tab-chats': {
+//          templateUrl: 'templates/chat-detail.html',
+//          controller: 'ChatDetailCtrl'
+//        }
+//      }
+//    })
+//
+//  .state('tab.friends', {
+//      url: '/friends',
+//      views: {
+//        'tab-friends': {
+//          templateUrl: 'templates/tab-friends.html',
+//          controller: 'FriendsCtrl'
+//        }
+//      }
+//    })
+//    .state('tab.friend-detail', {
+//      url: '/friend/:friendId',
+//      views: {
+//        'tab-friends': {
+//          templateUrl: 'templates/friend-detail.html',
+//          controller: 'FriendDetailCtrl'
+//        }
+//      }
+//    })
+//
+//  .state('tab.account', {
+//    url: '/account',
+//    views: {
+//      'tab-account': {
+//        templateUrl: 'templates/tab-account.html',
+//        controller: 'AccountCtrl'
+//      }
+//    }
+//  })
+  ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/login');
+
+});
---
+++ www/modules/home/HomeCtrl.js
@@ -0,0 +1,5 @@
+'use strict'
+angular
+.module('izy-companion.home', [])
+
+.controller('HomeCtrl', function($scope) {})
